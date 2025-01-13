7e302366f2c436496627735c336ce7fc5f081384
ahmedhabeeb73 <alnaib888@gmail.com>
2025-01-13 22:00:31 +0100

    Setting Up Service Providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Order;
+use Modules\Order\Providers\OrderServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
+    OrderServiceProvider::class,
 ];
---
+++ modules/Order/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/order', function () {
+    return 'This is the Order module.';
+});
